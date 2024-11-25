af98ef3ba7a55809428aa7558ac6489bc514f7c3
Jason Monroe <jwmonroe1@gmail.com>
2024-11-25 16:34:27 -0600

    adding easy js script

---
+++ js/palindrome.js
@@ -0,0 +1,46 @@
+/*
+|--------------------------------------------------------------------------
+| Palindrome
+|--------------------------------------------------------------------------
+| @link https://leetcode.com/problems/palindrome-number/submissions/1462830790/
+| 9. Palindrome Number
+|
+| Example 1:
+|
+| Input: x = 121
+| Output: true
+| Explanation: 121 reads as 121 from left to right and from right to left.
+| Example 2:
+|
+| Input: x = -121
+| Output: false
+| Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+| Example 3:
+|
+| Input: x = 10
+| Output: false
+| Explanation: Reads 01 from right to left. Therefore, it is not a palindrome.
+|
+| Constraints:
+|
+| -231 <= x <= 231 - 1
+|
+*/
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    str = x.toString()
+    const orig = str;
+    let rev = '';
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        rev += str[i];
+    }
+
+    return orig === rev;
+};
+
+isPalindrome(121)
