6ba77cfd81a0894abd07fdf8e0fd06c6c61cdd86
Jason Monroe <jwmfc9@gmail.com>
2025-01-22 20:54:10 -0600

    Adding all sys totals to see who won overall

--- py/ranked-choice-voting/voting_sys.py
+++ py/ranked-choice-voting/voting_sys.py
@@ -1,5 +1,13 @@
-# Voting System
-import math
+"""
+|--------------------------------------------------------------------------
+| Voting System
+|--------------------------------------------------------------------------
+| This is the base class for voting systems.
+| It contains the common methods
+| and attributes that are shared among the voting systems.
+"""
+
+# Import Libraries
 from helpers import show_output, MAX_CHOICES, NO_VOTE_VAL, FIRST_CHOICE_INDEX, map_id_to_candidate_index, sort_candidates
 
 class VotingSystem:
@@ -94,8 +102,6 @@ class VotingSystem:
         Determine the winner based on the highest total points.
         Determined by popularity
 
-        :param str rules: The rules to determine the winner.
-        :param int round_num: The current round number.
         :return: Candidates candidates: The list of candidates.
         """
 
@@ -145,60 +151,40 @@ class VotingSystem:
         return False
 
     @staticmethod
-    def get_highest_voted_candidate(candidates, data, choice=None):
+    def get_highest_voted_candidate(candidates, data_type, choice=None):
         """
         Get the candidate with the highest number of votes.
 
         :param list candidates: A list of candidates to compare.
-        :param string data: The data to compare.
+        :param string data_type: The data to compare.
         :param int choice: The choice to compare (optional)
 
         Returns:
            list: A list of candidates with the highest total points.
         """
 
-        if data == 'total':
+        if data_type == 'total':
             return [max(candidates, key=lambda c: c.total)]
         else:
             return [max(candidates, key=lambda c: c.votes[choice])]
 
-        """
-        print('Getting highest voted candidate...')
-        for candidate in candidates:
-            print(f'Debug:Candidate ID: {candidate.id} Total: {candidate.total}')
-        highest_total_pts = max(candidate.total for candidate in candidates)
-        print('Maximum total:', highest_total_pts)
-        return [candidate for candidate in candidates if candidate.total == highest_total_pts]
-        """
-
     @staticmethod
-    def get_lowest_voted_candidate(candidates, type, choice=None):
+    def get_lowest_voted_candidate(candidates, data_type, choice=None):
         """
         Get the candidate with the lowest number of votes.
 
         :param list candidates: A list of candidates to compare.
-        :param string type: The data type to compare.
+        :param string data_type: The data type to compare.
         :param int choice: The choice to compare (optional)
 
         Returns:
            list: A list of candidates with the lowest total points.
         """
 
-        if type == 'total':
+        if data_type == 'total':
             return [min(candidates, key=lambda c: c.total)]
         else:
             return [min(candidates, key=lambda c: c.votes[choice])]
-
-        """
-        return min(candidates, key=lambda c: c.total)
-
-        print('Getting lowest voted candidate...')
-        for candidate in candidates:
-            print(f'Debug:Candidate ID: {candidate.id} Total: {candidate.total}')
-        lowest_total_pts = min(candidate.total for candidate in candidates)
-        print('Minimum total:', lowest_total_pts)
-        return [candidate for candidate in candidates if candidate.total == lowest_total_pts]
-        """
 
     def remove_loser_from_ballot(self, loser):
         """
@@ -215,11 +201,6 @@ class VotingSystem:
                 if self.ballots[i][j] == loser.id:
                     self.ballots[i].pop(j) # Remove candidate from contention
 
-        #if show_output is True:
-            #print(f'* Removing loser {loser.id} from ballot.')
-            #print('Ballots after removal:', self.ballots)
-
-
     def get_pool_of_candidates(self):
         """
         Get the pool of candidates that are still in contention.
@@ -227,15 +208,13 @@ class VotingSystem:
         :returns: 
             list: A list of candidates that are still in contention.
         """
+
         return [candidate for candidate in self.candidates if candidate.is_winner is None]
 
     def reset_candidate_totals(self):
         """
         Reset the total points for each candidate so we can count the first choice each round.
         """
-        
-        #for i in range(0, len(self.candidates)):
-        #    self.candidates[i].total = 0
 
         for candidate in self.candidates:
             candidate.total = 0
@@ -251,22 +230,16 @@ class VotingSystem:
         :return: candidate: candidate with the highest/lowest votes.
         """
 
-        print(f'break_tie({candidates}, {mode}, {choice})')
-
-        #next_choice = choice + 1
         choice += 1
-        #print('next_choice:', next_choice, 'MAX_CHOICES:', MAX_CHOICES)
 
         if choice < MAX_CHOICES:
             while len(candidates) > 1 and choice < MAX_CHOICES:
 
                 if mode == 'min':
                     candidates = self.get_lowest_voted_candidate(candidates, 'votes', choice)
-                    #candidates = [min(candidates, key=lambda c: c.votes[choice])]
 
                 elif mode == 'max':
                     candidates = self.get_highest_voted_candidate(candidates, 'votes', choice)
-                    #candidates = [max(candidates, key=lambda c: c.votes[choice])]
 
                 # If there are still multiple candidates with the same number of votes, break the tie again.
                 candidates = self.break_tie(candidates, mode, choice)
@@ -282,136 +255,17 @@ class VotingSystem:
             print('Warning: Out of choices and still no clear winner.')
             return None
 
-            """
-            
-            if mode == 'min':
-
-                # Initialize min votes with first index.
-                min_votes = min(candidates, key=lambda x: x.votes[next_choice])
-                print('(min) Min votes:', min_votes)
-                min_votes = candidates[0].votes[next_choice]
-                print('Min votes:', min_votes)
-                exit()
-                min_candidate = candidates[0]
-
-                # Start with the next candidate in the list to get next minimum value
-                for i in range(1, len(candidates)):
-                    if candidates[i].votes[next_choice] < min_votes:
-                        min_votes = candidates[i].votes[next_choice]
-                        min_candidate = candidates[i]
-
-                if show_output is True:
-                    print('Returning minimum Candidate:', min_candidate.id)
-
-
-                    if candidate1[next_choice] < candidate2[next_choice]:
-                        return candidate1
-
-                    elif candidate1[next_choice] == candidate2[next_choice]:
-                        return self.break_tie(candidate1, candidate2, mode, next_place, MAX_CHOICES)
-
-                    else:
-                        return candidate2
-            elif mode == 'max':
-                print('\nMAX')
-                
-                
-                highest_candidates = max(candidates, key=lambda c: c.votes[next_choice])
-                
-                if (len(highest_candidates) > 1):
-                    print('Next Tie Breaker...')
-                    next_choice += 1
-                    max_votes = self.break_tie(highest_candidates, mode, next_choice)
-                    
-                #print('(max) Max votes:', max_votes.votes[next_choice], 'id:', max_votes.id,  'name:', max_votes.name)
-                #max_votes = candidates[0].votes[next_choice]
-                #print('Max votes:', max_votes)
-                #exit()
-                #if candidate1[next_place] > candidate2[next_place]:
-                #    return candidate1
-                #elif candidate1[next_place] == candidate2[next_place]:
-                #    return self.break_tie(candidate1, candidate2, mode, next_place, MAX_CHOICES)
-               #else:
-                #    return candidate2
-            """
- 
-    # If two candidates have the same min/max values, break the tie by looking at the next place voted for count.
-    # @todo - delete not needed
-    def break_tie2(self, candidate1, candidate2, mode, place):
-        """
-        Break the tie by comparing the next place voted for count.
-        Note: We can break tie with the highest or lowest total points.
-
-        :param candidate1:
-        :param candidate2:
-        :param mode:
-        :param place:
-        :return:
-        """
-        next_place = place + 1
-
-        if next_place < MAX_CHOICES:
-            if mode == 'min':
-                if candidate1[next_place] < candidate2[next_place]:
-                    return candidate1
-
-                elif candidate1[next_place] == candidate2[next_place]:
-                    return self.break_tie2(candidate1, candidate2, mode, next_place, MAX_CHOICES)
-
-                else:
-                    return candidate2
-            elif mode == 'max':
-                if candidate1[next_place] > candidate2[next_place]:
-                    return candidate1
-                elif candidate1[next_place] == candidate2[next_place]:
-                    return self.break_tie2(candidate1, candidate2, mode, next_place, MAX_CHOICES)
-                else:
-                    return candidate2
-        else:
-            return None
-
-
-    # Used in NextChoiceSystem
-    # @todo -remove
-    def break_tie_min(self, candidates, choice):
-        """
-        Break the tie by comparing the next place voted for count.
-        Note: This function uses multiple candidates.
-
-        :param candidates:
-        :param choice:
-        :return: candidate: candidate with the fewest votes.
-        """
-        print('show_output = ', show_output)
-        print('Candidate Count:', len(candidates))
-        for i in range(0, len(candidates)):
-            print('Debug Tie Breaker Candidate ID:', candidates[i].id, 'Total:', candidates[i].total)
-
-        print('line 195')
-        if show_output is True:
-            print('Attempting to break tie...')
-
-        next_choice = choice + 1
-
-        # Initialize min votes with first index.
-        min_votes = candidates[0].votes[next_choice]
-        min_candidate = candidates[0]
-
-        # Start with the next candidate in the list.
-        for i in range (1, len(candidates)):
-            print('Tie Breaker Candidate ID:', candidates[i].id, 'Votes:', candidates[i].votes[choice])
-            if next_choice < MAX_CHOICES:
-                if candidates[i].votes[next_choice] < min_votes:
-                    min_votes = candidates[i].votes[next_choice]
-                    min_candidate = candidates[i]
-
-        if show_output is True:
-            print('Returning minimum Candidate:', min_candidate.id)
-
-        return min_candidate
-
     # Used in Weighted System
     def break_tie_weighted(self, tied_candidates, choice):
+        """
+        Break the tie by comparing the next choice voted for count.
+        Note: We can break a tie with the highest or lowest total points.
+
+        :param [] tied_candidates:  List or candidates to compare.
+        :param int choice: Choice Number
+
+        :return: Int winner_id: The candidate with the highest/lowest votes.
+        """
         print('Breaking tie...')
         winner_id = None
         tie_breaker_total = 0
