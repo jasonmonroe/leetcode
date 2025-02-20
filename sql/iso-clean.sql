ebdce3582b34ef7eacfba4a26401e6d973359765
Jason Monroe <jwmonroe1@gmail.com>
2025-02-19 22:17:59 -0600

    typo fix, adding currency table, normalizing country table

--- sql/iso-clean.sql
+++ sql/iso-clean.sql
@@ -1,29 +1,33 @@
 /*
-|----------------------------------------------------------------------
-| International Organization for Standardization (ISO) Database Schema
-|----------------------------------------------------------------------
-| This script is a customized database for geographic information for
-| the global standards for trusted goods and services. ISO is an
-| independent, non-governmental international organization. It brings
-| global experts together to agree on the best ways of doing things.
-|
-| Procured by Jason Monroe (jason@jasonmonroe.com)
-| @link https://www.iso.org/home.html
-| @link https://www.iso.org/obp/ui/#search
-| @link https://www.iso.org/obp/ui/#iso:pub:PUB500001:en
-| @link https://en.wikipedia.org/wiki/List_of_ISO_639_language_codes
-| @link https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
-|
-| Note: Storing latitude and longitude coordinates for the Haversine
-| Formula.
+----------------------------------------------------------------------
+International Organization for Standardization (ISO) Database Schema
+----------------------------------------------------------------------
+This script is a customized database for geographic information for
+the global standards for trusted goods and services. ISO is an
+independent, non-governmental international organization. It brings
+global experts together to agree on the best ways of doing things.
+
+Procured by Jason Monroe (jason@jasonmonroe.com)
+
+Note: Storing latitude and longitude coordinates for the Haversine
+Formula.
+
+@link https://www.iso.org/home.html
+@link https://www.iso.org/obp/ui/#search
+@link https://www.iso.org/obp/ui/#iso:pub:PUB500001:en
+@link https://en.wikipedia.org/wiki/List_of_ISO_639_language_codes
+@link https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
+@link https://mkyong.com/java8/java-display-all-zoneid-and-its-utc-offset/
+@link https://www.timeanddate.com/time/zones/
 */
 
-CREATE DATABASE IF NOT EXISTS iso;
+CREATE DATABASE IF NOT EXISTS iso
 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 USE iso;
 
 CREATE TABLE location (
-    id INT unsigned UNIQUE NOT NULL AUTO_INCREMENT,
+    id INT UNSIGNED UNIQUE NOT NULL AUTO_INCREMENT,
     coordinates POINT NOT NULL SRID 4326, -- used for Geographic Information System
     SPATIAL INDEX(coordinates),  -- Enables efficient spatial queries
     latitude DECIMAL(11, 8), -- used for Haversine Formula (North to South)
@@ -33,62 +37,72 @@ CREATE TABLE location (
 );
 
 CREATE TABLE timezone (
-    id INT unsigned UNIQUE NOT NULL AUTO_INCREMENT,
+    id INT UNSIGNED UNIQUE NOT NULL AUTO_INCREMENT,
     standard_time_name VARCHAR(64) NOT NULL, -- i.e: Central Standard Time
     name VARCHAR(32) NOT NULL, -- i.e: America/Chicago
     offset VARCHAR(16), -- i.e: UTC+12:00
     group_offset BOOLEAN DEFAULT FALSE, -- used to group timezones by offset to prevent displaying all timezones
-    ordering INT UNSIGNED,     -- order by offset
+    ordering INT UNSIGNED, -- order by offset
+    status BOOLEAN DEFAULT TRUE,
+    PRIMARY KEY (id)
+    );
+
+CREATE TABLE language (
+    id INT UNSIGNED UNIQUE NOT NULL AUTO_INCREMENT,
+    name VARCHAR(32) NOT NULL,
+    local_name VARCHAR(32), -- name in local language
+    iso_code CHAR(2) NOT NULL, -- ISO 639 alpha-2 codes
     status BOOLEAN DEFAULT TRUE,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE language (
-    id INT unsigned UNIQUE NOT NULL AUTO_INCREMENT,
-    name VARCHAR(32) NOT NULL,
-    local_name VARCHAR(32), -- name in local language
-    iso_code VARCHAR(2) NOT NULL, -- ISO 639 alpha-2 codes
+CREATE TABLE currency (
+    id INT UNSIGNED UNIQUE NOT NULL AUTO_INCREMENT,
+    name VARCHAR(32) NOT NULL, -- i.e: United States Dollar
+    symbol VARCHAR(4) NOT NULL, -- i.e: $
+    iso_code VARCHAR(3) NOT NULL, -- i.e: USD
     status BOOLEAN DEFAULT TRUE,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE country (
-    id INT unsigned UNIQUE NOT NULL AUTO_INCREMENT,
+    id INT UNSIGNED UNIQUE NOT NULL AUTO_INCREMENT,
     short_name VARCHAR(64) UNIQUE NOT NULL,
     long_name VARCHAR(128) UNIQUE NOT NULL,
-    local_name VARCHAR(64) UNIQUE,
-    alpha_2_code VARCHAR(2) UNIQUE NOT NULL, -- iso code 2 chars
-    alpha_3_code VARCHAR(4) , -- iso code 3 chars
+    local_name VARCHAR(64),
+    alpha_2_code CHAR(2) UNIQUE NOT NULL, -- iso code 2 chars
+    alpha_3_code CHAR(3) UNIQUE NOT NULL, -- iso code 3 chars
     numeric_code INT(3) UNIQUE , -- ie: 840
     capital VARCHAR(64), -- optional
-    capital_id INT UNSIGNED,    
+    capital_id INT UNSIGNED,
     nationality_plural VARCHAR(32),
     nationality_singular VARCHAR(32),
-    continent VARCHAR(16), -- i.e: Africa, Europe, North America, South America, Oceania, Asia, Middle East
-    currency_code VARCHAR(4),
-    currency_symbol VARCHAR(2),
+    continent VARCHAR(16), -- i.e: Africa, Europe, North America, South America, Oceania, Asia, Middle East, Caribbean
+    currency_id INT UNSIGNED,
     comment VARCHAR(255),
-    location_id INT, -- if normalized
-    -- coordinates POINT NOT NULL SRID 4326, -- used for Geographic Information System (GIS)
-    -- SPATIAL INDEX(coordinates),  -- Enables efficient spatial queries
-    -- latitude DECIMAL(11, 8), -- used for Haversine Formula
-    -- longitude DECIMAL(11, 8), -- used for Haversine Formula
+    location_id INT UNSIGNED, -- if normalized
+    coordinates POINT NOT NULL SRID 4326, -- used for Geographic Information System (GIS)
+    SPATIAL INDEX(coordinates),  -- enables efficient spatial queries
+    latitude DECIMAL(11, 8), -- used for Haversine Formula
+    longitude DECIMAL(11, 8), -- used for Haversine Formula
     status BOOLEAN DEFAULT TRUE,
     PRIMARY KEY (id),
     FOREIGN KEY (location_id) REFERENCES location(id) ON UPDATE CASCADE ON DELETE CASCADE
+    FOREIGN KEY (capital_id) REFERENCES city(id) ON UPDATE CASCADE ON DELETE SET NULL
+    FOREIGN KEY (currency_id) REFERENCES currency(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE region (
-    id INT unsigned UNIQUE NOT NULL AUTO_INCREMENT,
-    country_id INT unsigned NOT NULL,
-    location_id INT,
-    language_id INT,
+    id INT UNSIGNED UNIQUE NOT NULL AUTO_INCREMENT,
+    country_id INT UNSIGNED NOT NULL,
+    location_id INT UNSIGNED,
+    language_id INT UNSIGNED,
     name VARCHAR(64) NOT NULL,
-    iso_code VARCHAR(8) NOT NULL, -- 3166-2 iso code
-    -- coordinates POINT NOT NULL SRID 4326, -- used for Geographic Information System
-    -- SPATIAL INDEX(coordinates), -- enables efficient spatial queries
-    -- latitude DECIMAL(11, 8), -- used for Haversine Formula
-    -- longitude DECIMAL(11, 8), -- used for Haversine Formula
+    iso_code VARCHAR(8) NOT NULL, -- 3166-2 iso sub-division code
+    coordinates POINT NOT NULL SRID 4326, -- used for Geographic Information System
+    SPATIAL INDEX(coordinates), -- enables efficient spatial queries
+    latitude DECIMAL(11, 8), -- used for Haversine Formula
+    longitude DECIMAL(11, 8), -- used for Haversine Formula
     status BOOLEAN DEFAULT TRUE,
     PRIMARY KEY (id),
     FOREIGN KEY (country_id) REFERENCES country(id) ON UPDATE CASCADE ON DELETE CASCADE,
@@ -99,7 +113,7 @@ CREATE TABLE region (
 -- Metropolitan area
 -- Used to bundle cities of a suburban areas to a metropolitan area
 CREATE TABLE metro (
-    id INT unsigned UNIQUE NOT NULL AUTO_INCREMENT,
+    id INT UNSIGNED UNIQUE NOT NULL AUTO_INCREMENT,
     city_id INT, -- used as a REFERENCES to the city proper of the metro (optional)
     name VARCHAR(64) NOT NULL,
     status BOOLEAN DEFAULT TRUE,
@@ -107,16 +121,16 @@ CREATE TABLE metro (
 );
 
 CREATE TABLE city (
-    id INT unsigned UNIQUE NOT NULL AUTO_INCREMENT,
+    id INT UNSIGNED UNIQUE NOT NULL AUTO_INCREMENT,
     region_id INT UNSIGNED NOT NULL,
-    metro_id INT UNSIGNED,    
+    metro_id INT UNSIGNED,
     timezone_id INT UNSIGNED,
-    location_id INT,
+    location_id INT UNSIGNED,
     name VARCHAR(64) NOT NULL,
-    -- coordinates POINT NOT NULL SRID 4326, -- used for Geographic Information System
-    -- SPATIAL INDEX(coordinates),  -- Enables efficient spatial queries
-    -- latitude DECIMAL(11, 8), -- used for Haversine Formula
-    -- longitude DECIMAL(11, 8), -- used for Haversine Formula
+    coordinates POINT NOT NULL SRID 4326, -- used for Geographic Information System
+    SPATIAL INDEX(coordinates),  -- Enables efficient spatial queries
+    latitude DECIMAL(11, 8), -- used for Haversine Formula
+    longitude DECIMAL(11, 8), -- used for Haversine Formula
     status BOOLEAN DEFAULT TRUE,
     PRIMARY KEY (id),
     FOREIGN KEY (region_id) REFERENCES region(id) ON UPDATE CASCADE ON DELETE CASCADE,
@@ -125,8 +139,7 @@ CREATE TABLE city (
     FOREIGN KEY (location_id) REFERENCES location(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
--- CREATE indices
--- Index FOREIGN KEYs, UNIQUE and joined columns
+-- CREATE indices on foreign keys, UNIQUE and joined columns
 
 -- Indices: location
 CREATE SPATIAL INDEX idx_location_coordinates ON location(coordinates);
@@ -149,12 +162,12 @@ CREATE INDEX idx_country_long_name ON country(long_name);
 CREATE INDEX idx_country_local_name ON country(local_name);
 CREATE INDEX idx_country_numeric_code ON country(numeric_code);
 CREATE INDEX idx_country_location ON country(location_id);
-CREATE INDEX idx_country_alpha_2_code ON country(alpha_2_code);
-CREATE INDEX idx_country_alpha_3_code ON country(alpha_3_code);
-CREATE INDEX idx_country_query ON country(short_name, location_id, alpha_2_code)
--- CREATE SPATIAL INDEX idx_country_coordinates ON country(coordinates);
--- CREATE INDEX idx_country_lon ON country(longitude);
--- CREATE INDEX idx_country_lat ON country(latitude);
+CREATE INDEX idx_country_capital ON country(capital_id);
+CREATE INDEX idx_country_currency ON currency(id);
+CREATE INDEX idx_country_query ON country(short_name, location_id, alpha_2_code);
+CREATE SPATIAL INDEX idx_country_coordinates ON country(coordinates);
+CREATE INDEX idx_country_lon ON country(longitude);
+CREATE INDEX idx_country_lat ON country(latitude);
 
 -- Indices: region
 CREATE INDEX idx_region_country ON region(country_id);
@@ -163,9 +176,9 @@ CREATE INDEX idx_region_name ON region(name);
 CREATE INDEX idx_region_iso_code ON region(iso_code);
 CREATE INDEX idx_region_location ON region(location_id);
 CREATE INDEX idx_region_query ON region(country_id, location_id, name);
--- CREATE SPATIAL INDEX idx_region_coordinates ON region(coordinates);
--- CREATE INDEX idx_region_lon ON region(longitude);
--- CREATE INDEX idx_region_lat ON region(latitude);
+CREATE SPATIAL INDEX idx_region_coordinates ON region(coordinates);
+CREATE INDEX idx_region_lon ON region(longitude);
+CREATE INDEX idx_region_lat ON region(latitude);
 
 -- Indices: metro
 CREATE INDEX idx_metro_city ON metro(city_id);
@@ -178,6 +191,6 @@ CREATE INDEX idx_city_timezone ON city(timezone_id);
 CREATE INDEX idx_city_location ON city(location_id);
 CREATE INDEX idx_city_name ON city(name);
 CREATE INDEX idx_city_query ON city(region_id, location_id, name, metro_id);
--- CREATE SPATIAL INDEX idx_city_coordinates ON city(coordinates);
--- CREATE INDEX idx_city_lon ON city(longitude);
--- CREATE INDEX idx_city_lat ON city(latitude);
+CREATE SPATIAL INDEX idx_city_coordinates ON city(coordinates);
+CREATE INDEX idx_city_lon ON city(longitude);
+CREATE INDEX idx_city_lat ON city(latitude);
